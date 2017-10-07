08f2500e871591e96d710bcd4f4efc01ef52be2c
PrajaktaDiware <prajktadiware1995@gmail.com>
2017-10-07 10:09:43 +0530

    CrudUsingWebapiWithAngular

---
+++ CrudOperation3rdOct/Models/PersonContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CrudOperation3rdOct.Controllers
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext() : base("name=MyConnection")
+        {
+
+        }
+        public DbSet<Person> PersonList
+        {
+            get; set;
+        }
+    }
+}
